06e34842b093c3dd7df64ad463b58bd5310024ff
Enrique Angola <eangola@uvm.edu>
2018-12-05 15:50:08 -0500

    wrote test for propershape

--- tests/test_readers/test_sympro_reader.py
+++ tests/test_readers/test_sympro_reader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Created : 04-12-2018
-Last Modified : Wed 05 Dec 2018 03:05:23 PM EST
+Last Modified : Wed 05 Dec 2018 03:49:45 PM EST
 Created By : Enrique D. Angola
 """
 
@@ -30,3 +30,10 @@ class TestSymproReader():
         fieldnames = self.object.get_fieldnames()
         assert fieldnames[0] == 'Timestamp' and\
                 fieldnames[-1] == 'Ch26_Vane_57.00m_S_GustDir_deg'
+    
+    def test_get_timeseries_properShapre_returnsTrue(self):
+
+        timeseries = self.object.get_timeseries('Ch1_Anem_58.00m_E_Gust_m/s',\
+                '2018-06-22 00:00:00','2018-06-22 00:20:00')
+
+        assert timeseries.shape == (3,2)
