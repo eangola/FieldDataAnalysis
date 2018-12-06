7b10d15bad9e8ecb8a3387860e0557dc47f3d3db
Enrique Angola <eangola@uvm.edu>
2018-12-06 12:48:57 -0500

    wrote test for apply filter and modified _init_ in readers to import sympro reader properly

--- fieldanalysis/readers/__init__.py
+++ fieldanalysis/readers/__init__.py
@@ -1,1 +1,3 @@
 #!/usr/bin/env python
+
+from fieldanalysis.readers.SymPro import SymPro
--- tests/test_readers/test_sympro_reader.py
+++ tests/test_readers/test_sympro_reader.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """
 Created : 04-12-2018
-Last Modified : Wed 05 Dec 2018 03:49:45 PM EST
+Last Modified : Thu 06 Dec 2018 12:48:12 PM EST
 Created By : Enrique D. Angola
 """
 
-from fieldanalysis.readers.SymPro import SymPro as reader
+from fieldanalysis.readers import SymPro as reader
 
 class TestSymproReader():
 
@@ -30,10 +30,19 @@ class TestSymproReader():
         fieldnames = self.object.get_fieldnames()
         assert fieldnames[0] == 'Timestamp' and\
                 fieldnames[-1] == 'Ch26_Vane_57.00m_S_GustDir_deg'
-    
+
     def test_get_timeseries_properShapre_returnsTrue(self):
 
         timeseries = self.object.get_timeseries('Ch1_Anem_58.00m_E_Gust_m/s',\
                 '2018-06-22 00:00:00','2018-06-22 00:20:00')
 
         assert timeseries.shape == (3,2)
+
+    def test_apply_filter_filteredDataframe_returnsTrue(self):
+
+        from fieldanalysis.filters import classic_filters as cf
+        filters = cf(self.object)
+        filters.generate_icing_filter(temp='Ch17_Analog_2.00m_S_Avg_C',vaneSD='Ch26_Vane_57.00m_S_SD_deg')
+        self.object.apply_filters(filters)
+
+        assert self.object.data.shape == (4,109)
