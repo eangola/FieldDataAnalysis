406a9c204f964f17bfbc6b703ca91df926f31fcc
Enrique Angola <eangola@uvm.edu>
2018-12-06 17:40:00 -0500

    added windspeed filtering and custom thresholds for filtering

--- fieldanalysis/filters/classic_filters.py
+++ fieldanalysis/filters/classic_filters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Created : 05-12-2018
-Last Modified : Thu 06 Dec 2018 12:21:43 PM EST
+Last Modified : Thu 06 Dec 2018 05:39:25 PM EST
 Created By : Enrique D. Angola
 """
 
@@ -28,8 +28,10 @@ class filters():
         self.data = reader.data
         self.reader = reader
         self.filtersDict = {'icing':None}
+        self.filtersDict['windspeed':None}
 
-    def generate_icing_filter(self,temp,vaneSD):
+    def generate_icing_filter(self,temp,vaneSD,tempThreshold = 3,\
+            vaneSDThreshold = 2):
         '''
         generate an icing filter given temperature and vane
         SD field.
@@ -37,5 +39,30 @@ class filters():
         temp = self.reader.get_data(temp)
         vaneSD = self.reader.get_data(vaneSD)
         if not self.filtersDict['icing']:
-            icingFilter = ((temp >= 3) & (vaneSD >= 2))
+            icingFilter = ((temp >= tempThreshold) &\
+                    (vaneSD >= vaneThreshold))
             self.filtersDict['icing'] = icingFilter
+
+
+    def generate_windspeed_filter(wsRef=None,wsThreshold = 3):
+        """
+
+
+        Parameters
+        ----------
+
+
+        Returns
+        -------
+
+
+        Examples
+        --------
+        >>>
+
+        """
+        wsRef = self.reader.get_data(wsRef)
+        if not self.filtersDict['windspeed']:
+            windspeedFilter = (wsRef >= wsThreshold)
+            self.filtersDict['windspeed'] = windspeedFilter
+
